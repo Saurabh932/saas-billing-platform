ef4c0ead0ed0b8f7e6b16ed723931db2efe2f6d5
Saurabh932 <saurabhmandhalkar03@gmail.com>
2026-06-25 01:50:19 +0530

    Signup form added and feature complete

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
     before_action :configure_sign_up_params, only: [ :create ]
 
     def create
-        result = AccountSignupService.call(sign_up_params)
+        result = AccountSignupService.call(sign_up_params.merge(account: params[:account]))
 
         if result.success?
             sign_in(result.user)
@@ -16,6 +16,6 @@ class RegistrationsController < Devise::RegistrationsController
 
     protected
     def configure_sign_up_params
-        devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, :org_name, :subdomain ])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, account: [ :org_name, :subdomain ] ])
     end
 end
